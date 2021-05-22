8e60e6365ab9381d60b5d559cc2385aa066d26c4
omarhashish <omarhashish20@gmail.com>
2021-05-22 19:06:04 +0200

    first commit

---
+++ src/config.js
@@ -0,0 +1,4 @@
+// Replace the string with your private API key for the News API.
+// You can get one for free if you register at: https://newsapi.org/register
+
+export const NEWS_API_KEY = "8538e270e70148a0bc86501a653f23a1";
